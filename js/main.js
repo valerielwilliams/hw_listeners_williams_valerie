ba271016305ca237757def76ddf3c5dd2f069ac5
Valerie Williams <valerielwilliams@ufl.edu>
2017-03-24 19:07:15 -0400

    add addEventListener method to h1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+var heading = document.getElementsByTagName('h1');
 
+heading.addEventListener('click', function () {
+    var newParagraph = document.createElement('p');
+    newParagraph.innerHTML = '<p>This is click number x</p>';
+    document.getElementsByTagName('body')[0].appendChild(newParagraph);
+});
