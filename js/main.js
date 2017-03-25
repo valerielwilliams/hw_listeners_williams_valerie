4158f2bf71a5c5ee1fe04ce5b9d5364485ec1f30
Valerie Williams <valerielwilliams@ufl.edu>
2017-03-25 17:50:48 -0400

    add JavaScript to change color of second paragraph
    The JavaScript code listens for a mouseover event, and changes the
    color of the paragraph that reads 'a good day' from dark gray to blue.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@ forgood.addEventListener('click', function () {   //This listens for a click on 
     newParagraph.innerHTML = '<p>for a good day</p>'; //completes the sentence with 'for a good day'
     document.getElementsByTagName('body')[0].appendChild(newParagraph);
 });
+
+var goodday = document.getElementById('good'); //This listens for a mouse hover over 'a good day' paragraph.
+goodday.addEventListener('mouseover', function (event) { //Upon hearing the hover, the paragraph turns blue.
+    event.target.style.color = '#2981b5';
+});
