13de1f87ff79782e4340199ae5c6886537b888fd
Valerie Williams <valerielwilliams@ufl.edu>
2017-03-25 14:28:25 -0400

    add IDs to fields to use in JavaScript
    I added IDs to the fields so that I could isolate them in variables
    using the document.getElementById method. I then reference those
    variables in the console.log command.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 var form = document.getElementById('form1');
-form = addEventListener('submit', function (eventInfo) {
+var userFirstName = document.getElementById('firstName');
+var userLastName = document.getElementById('lastName');
+var userEmail = document.getElementById('email');
+var userMessage = document.getElementById('message');
+
+form.addEventListener('submit', function (eventInfo) {
     eventInfo.preventDefault();
-    console.log(form).getAttribute('value');
+    console.log('The firstName is:', userFirstName.value);
+    console.log('The lastName is:', userLastName.value);
+    console.log('The email is:', userEmail.value);
+    console.log('The message is:', userMessage.value);
 });
