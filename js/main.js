f91ceaa6fc474c9c5c4440a9e2d2e0e6e4910de0
Valerie Williams <valerielwilliams@ufl.edu>
2017-03-25 19:15:29 -0400

    return master branch to original state
    I really messed up trying to fix an old commit. Apparently, I merged
    this branch with the listener branch. I don't want to talk about it.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-var heading = document.getElementById('mainHeading');
-var a = 0;
 
-heading.addEventListener('click', function () {
-    var newParagraph = document.createElement('p');
-    newParagraph.innerHTML = '<p>This is click number:</p>' + (++a);
-    document.getElementsByTagName('body')[0].appendChild(newParagraph);
-});
